bcbee034001205106dbc3fa9946dc502b2944362
cr0c0 <hyde69ciel@gmail.com>
2024-05-05 00:38:49 +0900

    #240505 스프링부트 프로젝트 생성

---
+++ src/main/java/me/croco/onulmohaji/OnulMohajiApplication.java
@@ -0,0 +1,11 @@
+package me.croco.onulmohaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnulMohajiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnulMohajiApplication.class, args);
+    }
+}
