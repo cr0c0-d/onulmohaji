13399d066c3f1b8e2d35fe0b98617205b4706552
cr0c0 <hyde69ciel@gmail.com>
2024-05-16 23:52:39 +0900

    #240516 Member 도메인 추가, TokenProvider 추가

---
+++ src/main/java/me/croco/onulmohaji/member/service/MemberService.java
@@ -0,0 +1,50 @@
+package me.croco.onulmohaji.member.service;
+
+import lombok.RequiredArgsConstructor;
+import me.croco.onulmohaji.member.domain.Member;
+import me.croco.onulmohaji.member.dto.MemberAddRequest;
+import me.croco.onulmohaji.member.repository.MemberRepository;
+import me.croco.onulmohaji.util.Authorities;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 email : " + username));
+    }
+
+    public Member findById(Long id) {
+        return memberRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 id : " + id));
+    }
+
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 email : " + email));
+    }
+
+    public Long saveMember(MemberAddRequest request) {
+
+        if(memberRepository.existsByEmail(request.getEmail())) {    // 이메일 중복확인
+            return null;
+        }
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return memberRepository.save(
+                        Member.builder()
+                                .email(request.getEmail())
+                                .nickname(request.getNickname())
+                                .password(bCryptPasswordEncoder.encode(request.getPassword()))
+                                .authorities(Authorities.ROLE_USER)
+                                .build()
+                )
+                .getId();
+    }
+}
