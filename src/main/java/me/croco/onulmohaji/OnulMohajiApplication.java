715804f69b7ae159a2cebc99fc6b07781d39379c
cr0c0 <hyde69ciel@gmail.com>
2024-06-08 22:47:36 +0900

    #240608 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/me/croco/onulmohaji/OnulMohajiApplication.java
+++ src/main/java/me/croco/onulmohaji/OnulMohajiApplication.java
@@ -2,8 +2,10 @@ package me.croco.onulmohaji;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OnulMohajiApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnulMohajiApplication.class, args);
