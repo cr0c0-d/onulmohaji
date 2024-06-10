09fdfc2f029fdc9eb902ecf88a14a1960b632f47
cr0c0 <hyde69ciel@gmail.com>
2024-06-10 20:09:16 +0900

    #240610 facility 목록 조회시 DB에 저장된 facility가 10개 미만인 경우에만 카카오맵 api 사용

--- src/main/java/me/croco/onulmohaji/facility/service/FacilityService.java
+++ src/main/java/me/croco/onulmohaji/facility/service/FacilityService.java
@@ -23,18 +23,24 @@ public class FacilityService {
     }
 
     public List<Facility> findLocalFacilityList(String keyword, Double latitude, Double longitude) {
-        CompletableFuture.runAsync(() -> {
-            facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("이색데이트", null, latitude, longitude));
-            facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("실내놀거리", null, latitude, longitude));
-            facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("테마파크", null, latitude, longitude));
-            facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("테마카페", null, latitude, longitude));
-        });
+        
 
         List<Facility> facilityList = null;
         if(keyword == null) {
             facilityList = facilityRepository.findDefaultFacilityList(latitude, longitude);
+            
+            // DB에 저장된 facility가 10개 미만인 경우 카카오맵에서 가져와서 저장하기
+            if(facilityList.size() < 10) {
+                CompletableFuture.runAsync(() -> {
+                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("이색데이트", null, latitude, longitude));
+                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("실내놀거리", null, latitude, longitude));
+                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("테마파크", null, latitude, longitude));
+                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("테마카페", null, latitude, longitude));
+                });
+            }
         } else {
-            facilityList = facilityRepository.findFacilityListByKeyword(keyword, latitude, longitude);
+            //facilityList = facilityRepository.findFacilityListByKeyword(keyword, latitude, longitude);
+            facilityList = facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory(keyword, null, latitude, longitude));
         }
         return facilityList;
     }
