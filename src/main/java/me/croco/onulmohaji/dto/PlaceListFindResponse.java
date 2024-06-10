9570d4e9edfbe299b87f82f4392dc42bd2b1bd07
cr0c0 <hyde69ciel@gmail.com>
2024-06-10 20:07:44 +0900

    #240610 place 목록 조회 dto - route 조회시 변수명과 일치화

--- src/main/java/me/croco/onulmohaji/dto/PlaceListFindResponse.java
+++ src/main/java/me/croco/onulmohaji/dto/PlaceListFindResponse.java
@@ -13,25 +13,27 @@ import java.time.format.DateTimeFormatter;
 @Getter
 @Setter
 public class PlaceListFindResponse {
-    private String id;
+    private String placeId;
     private int categoryId;
     private String category;
-    private String title;
+    private String placeName;
+    private String placeType;
+    private String placeTypeName;
     private String address_short;
     private String address;
     private boolean preRegister;
     private String preRegisterStartDate;
     private String preRegisterEndDate;
     private String preRegisterLink;
-    private String thumbnails;
+    private String thumbnail;
     private Double latitude;
     private Double longitude;
     private String startDate;
     private String endDate;
 
     public PlaceListFindResponse(Popupstore popupstore) {
-        this.id = String.valueOf(popupstore.getStoreId());
-        this.title = HtmlUtils.htmlUnescape(popupstore.getTitle());
+        this.placeId = String.valueOf(popupstore.getStoreId());
+        this.placeName = HtmlUtils.htmlUnescape(popupstore.getTitle());
         this.categoryId = popupstore.getCategoryId();
         this.address_short = popupstore.getTopLevelAddress();
         this.address = popupstore.getAddress();
@@ -39,34 +41,40 @@ public class PlaceListFindResponse {
         this.preRegisterStartDate = popupstore.getPreRegisterStartDate();
         this.preRegisterEndDate = popupstore.getPreRegisterEndDate();
         this.preRegisterLink = popupstore.getPreRegisterLink();
-        this.thumbnails = popupstore.getThumbnails();
+        this.thumbnail = popupstore.getThumbnails();
         this.latitude = popupstore.getLatitude();
         this.longitude = popupstore.getLongitude();
         this.startDate = popupstore.getStartDate();
         this.endDate = popupstore.getEndDate();
+        this.placeType = "popup";
+        this.placeTypeName = "팝업스토어";
     }
 
     public PlaceListFindResponse(Exhibition exhibition) {
-        this.id = String.valueOf(exhibition.getSeq());
-        this.title = HtmlUtils.htmlUnescape(exhibition.getTitle());
+        this.placeId = String.valueOf(exhibition.getSeq());
+        this.placeName = HtmlUtils.htmlUnescape(exhibition.getTitle());
         this.startDate = LocalDate.parse(exhibition.getStartDate()).format(DateTimeFormatter.ofPattern("yy-MM-dd"));
         this.endDate = LocalDate.parse(exhibition.getEndDate()).format(DateTimeFormatter.ofPattern("yy-MM-dd"));
         this.address_short = exhibition.getArea() + " " + exhibition.getPlace();
         this.category = exhibition.getRealmName();
-        this.thumbnails = exhibition.getThumbnail();
+        this.thumbnail = exhibition.getThumbnail();
         this.latitude = exhibition.getGpsX();
         this.longitude = exhibition.getGpsY();
+        this.placeType = "exhibition";
+        this.placeTypeName = "전시회/공연";
     }
 
     public PlaceListFindResponse(Festival festival) {
-        this.id = festival.getId();
-        this.title = festival.getTitle();
+        this.placeId = festival.getId();
+        this.placeName = festival.getTitle();
         this.address_short = festival.getAreaNm();
         this.address = festival.getAddress();
-        this.thumbnails = festival.getThumbnail();
+        this.thumbnail = festival.getThumbnail();
         this.latitude = festival.getLatitude();
         this.longitude = festival.getLongitude();
         this.startDate = festival.getStartDate();
         this.endDate = festival.getEndDate();
+        this.placeType = "festival";
+        this.placeTypeName = "축제";
     }
 }
