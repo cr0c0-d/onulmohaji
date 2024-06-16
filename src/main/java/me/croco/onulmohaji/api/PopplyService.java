e9d553b82898d96cfacfad42848fd038acaaa386
cr0c0 <hyde69ciel@gmail.com>
2024-06-16 22:50:12 +0900

    #240616 팝업스토어 - 팝플리 정보 조회시 1일 단위로 30일간의 정보 조회하도록 변경, 메모리 크기 변경

--- src/main/java/me/croco/onulmohaji/api/PopplyService.java
+++ src/main/java/me/croco/onulmohaji/api/PopplyService.java
@@ -10,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,32 +28,47 @@ public class PopplyService {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate today = LocalDate.now();
         LocalDate oneMonthLater = today.plusMonths(1);
+        List<PopplyPopupstoreFindResponse> list = new ArrayList<>();
 
         WebClient webClient = WebClient.builder()
+                .exchangeStrategies(builder -> builder.codecs(clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs().maxInMemorySize(-1)))
                 .baseUrl(POPPLY_BASE_URL)
                 .build();
 
-        String response = webClient.get()
-                            .uri(uriBuilder -> uriBuilder.path(POPPLY_FIND_LIST_URL)
-                                    .queryParam("startDate", dateFormatter.format(today))
-                                    .queryParam("endDate", dateFormatter.format(oneMonthLater))
-                                    .build()
-                            )
-                            .retrieve()
-                            .bodyToMono(String.class)
-                            .block();
+        while(true) {
 
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            JsonNode rootNode = mapper.readTree(response);
-            JsonNode dataNode = rootNode.path("data");
-            if (!dataNode.isMissingNode()) { // 'data' 필드가 존재하는지 확인
-                List<PopplyPopupstoreFindResponse> stores = mapper.convertValue(dataNode, new com.fasterxml.jackson.core.type.TypeReference<List<PopplyPopupstoreFindResponse>>() {
-                });
-                popupstoreService.savePopupstoreInfo(stores);
+            LocalDate queryDate = today;
+            String response = webClient.get()
+                    .uri(uriBuilder -> uriBuilder.path(POPPLY_FIND_LIST_URL)
+                            .queryParam("startDate", dateFormatter.format(queryDate))
+                            .queryParam("endDate", dateFormatter.format(queryDate))
+                            .build()
+                    )
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                JsonNode rootNode = mapper.readTree(response);
+                JsonNode dataNode = rootNode.path("data");
+                if (!dataNode.isMissingNode()) { // 'data' 필드가 존재하는지 확인
+                    List<PopplyPopupstoreFindResponse> stores = mapper.convertValue(dataNode, new com.fasterxml.jackson.core.type.TypeReference<List<PopplyPopupstoreFindResponse>>() {
+                    });
+                    List<PopplyPopupstoreFindResponse> filtered = stores.stream().filter(responseStore -> list.stream().noneMatch(store -> store.getStoreId().equals(responseStore.getStoreId()))).toList();
+                    list.addAll(filtered);
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            if(queryDate.isEqual(oneMonthLater)) {
+                break;
+            } else {
+                today = today.plusDays(1);
+            }
         }
+        popupstoreService.savePopupstoreInfo(list);
+
     }
 }
