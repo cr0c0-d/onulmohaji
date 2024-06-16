9053e3559c46d8c7cc04cb21ec22a85dac4aeb15
cr0c0 <hyde69ciel@gmail.com>
2024-06-16 22:50:41 +0900

    #240616 팝업스토어 상세정보 응답 dto 변경

--- src/main/java/me/croco/onulmohaji/popupstore/dto/PopupDetailFindResponse.java
+++ src/main/java/me/croco/onulmohaji/popupstore/dto/PopupDetailFindResponse.java
@@ -6,7 +6,9 @@ import me.croco.onulmohaji.popupstore.domain.Popupstore;
 import me.croco.onulmohaji.popupstore.domain.PopupstoreDetail;
 import me.croco.onulmohaji.popupstore.domain.PopupstoreImage;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Setter
 @Getter
@@ -18,8 +20,12 @@ public class PopupDetailFindResponse {
     private String endDate;
     private Double latitude;
     private Double longitude;
+    private String thumbnail;
     private List<String> imageList;
     private String address;
+    private Map<String, String> preRegisterInfo;
+    private String workingTime;
+
 
     private String contents1;
     private String contents2;
@@ -33,6 +39,7 @@ public class PopupDetailFindResponse {
     private int adult;
     private int wifi;
 
+
     public PopupDetailFindResponse(Popupstore popupstore, PopupstoreDetail popupstoreDetail, List<PopupstoreImage> images) {
         this.placeId = String.valueOf(popupstore.getStoreId());
         this.placeName = popupstore.getName();
@@ -40,8 +47,19 @@ public class PopupDetailFindResponse {
         this.endDate = popupstore.getEndDate();
         this.latitude = popupstore.getLatitude();
         this.longitude = popupstore.getLongitude();
+        this.thumbnail = popupstore.getThumbnails();
         this.imageList = images.stream().map(PopupstoreImage::getUrl).toList();
         this.address = popupstore.getAddress();
+
+        if(popupstore.isPreRegister()) {
+            Map<String, String> preRegisterInfo = new HashMap<>();
+            preRegisterInfo.put("startDate", popupstore.getPreRegisterStartDate());
+            preRegisterInfo.put("endDate", popupstore.getPreRegisterEndDate());
+            preRegisterInfo.put("link", popupstore.getPreRegisterLink());
+            this.preRegisterInfo = preRegisterInfo;
+        }
+
+        this.workingTime = popupstore.getWorkingTime().replace("_date", "Date");
 
         this.contents1 = popupstoreDetail.getNotice();
         this.contents2 = popupstoreDetail.getContents();
