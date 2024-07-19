4cabee0987d0964f8210297c73603e82fdca0a04
cr0c0 <hyde69ciel@gmail.com>
2024-07-19 23:47:06 +0900

    #240719 Faciltiy 조회 로직 정리 및 검색어 적용

--- src/main/java/me/croco/onulmohaji/facility/service/FacilityService.java
+++ src/main/java/me/croco/onulmohaji/facility/service/FacilityService.java
@@ -9,8 +9,7 @@ import me.croco.onulmohaji.facility.dto.FacilityListFindResponse;
 import me.croco.onulmohaji.facility.repository.FacilityRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -58,36 +57,44 @@ public class FacilityService {
          * 식당, 테마카페, 관광명소, 문화예술, 실내놀거리
          */
         if(keyword == null) {
-            List<FacilityFindResponse> food = facilityRepository.findFacilityListByCategory("음식점", latitude, longitude).stream().map(FacilityFindResponse::new).toList();
-            List<FacilityFindResponse> cafe = facilityRepository.findFacilityListByCategory("테마카페", latitude, longitude).stream().map(FacilityFindResponse::new).toList();
-            List<FacilityFindResponse> landmark = facilityRepository.findFacilityListByCategory("관광,명소", latitude, longitude).stream().map(FacilityFindResponse::new).toList();
-            List<FacilityFindResponse> art = facilityRepository.findFacilityListByCategory("문화,예술", latitude, longitude).stream().map(FacilityFindResponse::new).toList();
-            List<FacilityFindResponse> playground = facilityRepository.findFacilityListByCategory("실내놀거리", latitude, longitude).stream().map(FacilityFindResponse::new).toList();
 
-            responses.add(new FacilityListFindResponse("음식점", food));
-            responses.add(new FacilityListFindResponse("테마카페", cafe));
-            responses.add(new FacilityListFindResponse("관광명소", landmark));
-            responses.add(new FacilityListFindResponse("문화예술", art));
-            responses.add(new FacilityListFindResponse("실내놀거리", playground));
+            Map<String, String> categoryList = new HashMap<>();
+
+            // 표시이름 : 검색어
+            categoryList.put("음식점", "음식점");
+            categoryList.put("테마카페", "테마카페");
+            categoryList.put("관광명소", "관광,명소");
+            categoryList.put("문화예술", "문화,예술");
+            categoryList.put("실내놀거리", "실내놀거리");
+
+            Iterator<String> it = categoryList.keySet().iterator();
+            String typeName = it.next();
+
+            while(true) {
+                List<FacilityFindResponse> facilityList = facilityRepository.findFacilityListByCategory(categoryList.get(typeName), latitude, longitude).stream().map(FacilityFindResponse::new).toList();
+                responses.add(new FacilityListFindResponse(typeName, facilityList));
+
+                if(it.hasNext()) {
+                    typeName = it.next();
+                } else {
+                    break;
+                }
+            }
+
+            responses.stream().forEach(facilityListFindResponse -> {
+                if(facilityListFindResponse.getFacilityList().size() < 50) {
+                    CompletableFuture.runAsync(() -> {
+                        facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory(facilityListFindResponse.getTypeName(), null, latitude, longitude));
+                    });
+                }
+            });
 
 
-//            // DB에 저장된 facility가 100개 미만인 경우 카카오맵에서 가져와서 저장하기
-//            if(facilityList.size() < 100) {
-//                CompletableFuture.runAsync(() -> {
-//                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("이색데이트", null, latitude, longitude));
-//                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("실내놀거리", null, latitude, longitude));
-//                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("테마파크", null, latitude, longitude));
-//                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("테마카페", null, latitude, longitude));
-//                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("문화", null, latitude, longitude));
-//                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("산책", null, latitude, longitude));
-//                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("레저", null, latitude, longitude));
-//                    facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory("명소", null, latitude, longitude));
-//                });
-//            }
         } else {
-            //facilityList = facilityRepository.findFacilityListByKeyword(keyword, latitude, longitude);
-            //facilityList = facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory(keyword, null, latitude, longitude));
+           List<Facility> facilityList = facilityRepository.saveAll(kakaoLocalService.getLocalListByCategory(keyword, null, latitude, longitude));
+           responses.add(new FacilityListFindResponse("기타 장소", facilityList.stream().map(FacilityFindResponse::new).toList()));
         }
+
         return responses;
     }
 
