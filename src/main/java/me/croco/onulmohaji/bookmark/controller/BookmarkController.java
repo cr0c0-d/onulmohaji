523b668ed95340929e75e684f1de8490a760ff8d
cr0c0 <hyde69ciel@gmail.com>
2024-08-13 00:22:49 +0900

    #240813 Bookmark 생성 및 삭제 api 작성

--- src/main/java/me/croco/onulmohaji/bookmark/controller/BookmarkController.java
+++ src/main/java/me/croco/onulmohaji/bookmark/controller/BookmarkController.java
@@ -1,7 +1,14 @@
 package me.croco.onulmohaji.bookmark.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import me.croco.onulmohaji.bookmark.dto.BookmarkAddRequest;
 import me.croco.onulmohaji.bookmark.service.BookmarkService;
+import me.croco.onulmohaji.member.domain.Member;
+import me.croco.onulmohaji.member.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
+    private final MemberService memberService;
 
+    @PostMapping("/api/bookmark")
+    public ResponseEntity<Boolean> toggleBookmark(@RequestBody BookmarkAddRequest addRequest, HttpServletRequest request) {
+        Member loginMember = memberService.getLoginMember(request);
+        Boolean bookmarkYn = bookmarkService.toggleBookmark(addRequest, loginMember);
+
+        return ResponseEntity.ok()
+                .body(bookmarkYn);
+
+    }
 
 }
--- src/main/java/me/croco/onulmohaji/bookmark/repository/BookmarkRepository.java
+++ src/main/java/me/croco/onulmohaji/bookmark/repository/BookmarkRepository.java
@@ -3,5 +3,8 @@ package me.croco.onulmohaji.bookmark.repository;
 import me.croco.onulmohaji.bookmark.domain.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    Optional<Bookmark> findByUserIdAndPlaceTypeAndPlaceId(Long userId, String placeType, String placeId);
 }
