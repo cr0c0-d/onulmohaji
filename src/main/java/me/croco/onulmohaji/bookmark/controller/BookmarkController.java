0b080315d4ecd178078bece2dcffb3cf7270a6dc
cr0c0 <hyde69ciel@gmail.com>
2024-10-14 23:56:44 +0900

    #241014 Bookmark Place 목록 조회 API 추가

--- src/main/java/me/croco/onulmohaji/bookmark/controller/BookmarkController.java
+++ src/main/java/me/croco/onulmohaji/bookmark/controller/BookmarkController.java
@@ -6,8 +6,14 @@ import me.croco.onulmohaji.bookmark.domain.Bookmark;
 import me.croco.onulmohaji.bookmark.dto.BookmarkAddRequest;
 import me.croco.onulmohaji.bookmark.dto.BookmarkListFindResponse;
 import me.croco.onulmohaji.bookmark.service.BookmarkService;
+import me.croco.onulmohaji.dto.PlaceListFindResponse;
+import me.croco.onulmohaji.exhibition.service.ExhibitionService;
+import me.croco.onulmohaji.facility.dto.FacilityFindResponse;
+import me.croco.onulmohaji.facility.service.FacilityService;
+import me.croco.onulmohaji.festival.service.FestivalService;
 import me.croco.onulmohaji.member.domain.Member;
 import me.croco.onulmohaji.member.service.MemberService;
+import me.croco.onulmohaji.popupstore.service.PopupstoreService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +28,10 @@ public class BookmarkController {
 
     private final BookmarkService bookmarkService;
     private final MemberService memberService;
+    private final FestivalService festivalService;
+    private final ExhibitionService exhibitionService;
+    private final PopupstoreService popupstoreService;
+    private final FacilityService facilityService;
 
     @PostMapping("/api/bookmark")
     public ResponseEntity<Boolean> toggleBookmark(@RequestBody BookmarkAddRequest addRequest, HttpServletRequest request) {
@@ -40,6 +50,26 @@ public class BookmarkController {
         return ResponseEntity.ok()
                 .body(bookmarkList.stream().map(BookmarkListFindResponse::new).toList());
 
+    }
+
+    @GetMapping("/api/bookmark/list")
+    public ResponseEntity<List<Object>> getBookmarkPlaceList(HttpServletRequest request) {
+        Member loginMember = memberService.getLoginMember(request);
+        List<Bookmark> bookmarkList = bookmarkService.getBookmarkList(loginMember);
+
+        return ResponseEntity.ok()
+                .body(bookmarkList.stream().map(bookmark -> {
+                    return switch (bookmark.getPlaceType()) {
+                        case "festival" ->
+                                new PlaceListFindResponse(festivalService.findFestivalById(bookmark.getPlaceId()), (double) 0, (double) 0);
+                        case "exhibition" ->
+                                new PlaceListFindResponse(exhibitionService.findExhibitionById(bookmark.getPlaceId()), (double) 0, (double) 0);
+                        case "popup" ->
+                                new PlaceListFindResponse(popupstoreService.findPopupstoreById(bookmark.getPlaceId()), (double) 0, (double) 0);
+                        default ->
+                                new FacilityFindResponse(facilityService.findFacilityById(bookmark.getPlaceId()), (double) 0, (double) 0);
+                    };
+                }).toList());
     }
 
 }
